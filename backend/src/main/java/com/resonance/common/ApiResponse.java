2e61fe87fcd58cbb3aecafdadc8377df529c1811
handzhaoIscoming <handzhao2318593985@gmail.com>
2026-02-27 00:12:47 +0800

    feat(task_server): 重构后端架构，集成 Redis 并在 api-test 增加测试脚本

---
+++ backend/src/main/java/com/resonance/common/ApiResponse.java
@@ -0,0 +1,23 @@
+package com.resonance.common;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ApiResponse<T> {
+
+    private int code;       // 0 表示成功，非 0 表示失败
+    private String message; // 提示信息
+    private T data;         // 真正的业务数据
+
+    public ApiResponse() {
+        
+    }
+
+    public ApiResponse(int code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+}
