c97b5e5eac0ef0e912ff325ec36fc4444c062322
handzhaoIscoming <handzhao2318593985@gmail.com>
2026-03-10 17:44:50 +0800

    feat(room): 添加记录日志

--- backend/src/main/java/com/resonance/aop/RequireHostAspect.java
+++ backend/src/main/java/com/resonance/aop/RequireHostAspect.java
@@ -10,6 +10,8 @@ import org.aspectj.lang.annotation.Before;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Aspect
 @Component
@@ -18,20 +20,19 @@ public class RequireHostAspect {
     @Autowired
     @Lazy
     private RoomService roomService;
-
+    private static final Logger log = LoggerFactory.getLogger(RequireHostAspect.class);
     @Before("@annotation(com.resonance.annotation.RequireHost)")
     public void checkHostPermission(JoinPoint joinPoint){
-
         //1.获取被拦截方法的参数
-
         Object[] args = joinPoint.getArgs();
 
         if(args == null || args.length < 2){
+            log.error("❌ 系统错误：@RequireHost 拦截到的方法参数缺失！");
             throw new RuntimeException("内部错误：校验方法参数错误");
         }
         String roomId = (String)args[0];
         String userId = (String)args[1];
-
+        log.warn("⚠️ 拦截异常操作：用户 [{}] 试图操作不存在或已解散的房间 [{}]", userId, roomId);
         Room room = roomService.getRoom(roomId);
 
         if(room == null){
